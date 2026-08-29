e19158fe580880c076cd21224326f221cb28537e
muhammadhammad249 <muhammadhammadjatoi65@gmail.com>
2026-08-28 21:40:49 -0700

    Fix Vercel backend entrypoint

--- backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  earlyAccess: true,
-  schema: '../database/prisma/schema.prisma',
-  datasource: {
-    url: 'file:../database/prisma/dev.db',
-  },
-});
